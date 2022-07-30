8b48193f6d7caeaa3a357d3207832bbf0b8daa22
math <blackcloister@gmail.com>
2022-07-30 16:38:27 +0900

    stable

---
+++ src/main_2layer.py
@@ -0,0 +1,27 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Dense, Flatten, Lambda
+from keras.backend import argmax, cast
+from keras.utils import to_categorical
+
+from keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+model = Sequential([
+    Conv2D(16, (5,5), strides=(4,4), activation='tanh', input_shape=(28,28,1)),    # Layer H1
+    Flatten(),
+    Dense(12 * 16),  # Layer H2
+    Dense(10, activation='softmax') # Output Layer
+])
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10, batch_size=16)
+
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=16)
